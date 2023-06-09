b7dea4b81d2bf85e2ea0a117474acfe62f07c1a4
mojjoiv <daltonongeche@gmail.com>
2023-06-09 06:46:42 +0300

    mongodb connection and implementation of the api from a public api

---
+++ airbnb-clone/client/tailwind.config.cjs
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors:{
+        primary: '#329a9a'
+      },
+       wave: {
+          '0%': { transform: 'rotate(0.0deg)' },
+          '10%': { transform: 'rotate(14deg)' },
+          '20%': { transform: 'rotate(-8deg)' },
+          '30%': { transform: 'rotate(14deg)' },
+          '40%': { transform: 'rotate(-4deg)' },
+          '50%': { transform: 'rotate(10.0deg)' },
+          '60%': { transform: 'rotate(0.0deg)' },
+          '100%': { transform: 'rotate(0.0deg)' },
+        },
+    },
+  },
+  plugins: [],
+};
